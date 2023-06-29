6eb096e018e118194681020a22be37703f5d2cb5
vskartg <135719782+vskartg@users.noreply.github.com>
2023-06-29 12:55:03 -0400

    Update newplace.js

--- docker/app2/newplace.js
+++ docker/app2/newplace.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/karthik', (req, res) => {
-  res.send(", Hello, This is karthik!")
+app.get('/harshitha', (req, res) => {
+  res.send(", Hello, This is harshitha!")
 })
 
 app.listen(port, () => {
